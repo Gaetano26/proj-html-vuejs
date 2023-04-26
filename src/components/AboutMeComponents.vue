ec686b533a7b02c207756365453732364b474e7e
Gaetano26 <gaetano.spampinato@outlook.it>
2023-04-26 18:08:03 +0200

    AboutMeComponent complete

---
+++ src/components/AboutMeComponents.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="col pad">
+        <h6>About me</h6>
+        <h3 class="mt-4">How to Be an Alpha Male: The 15 Habits of True Alpha </h3>
+    </div>
+    <div class="col pt-5 pad2">
+        <h6>Want to learn how to be <span>an alpha male</span>? How to be more Masculine?
+        How to be the man you've always wanted to be?</h6>
+        <p>Your goal is to show the consistency of business, cars values, abd customers cars.
+          Punctuality, responsability, Individuality, accuracy, enthusiasm - you have to clearly explain how these qualities
+          are applicable to your work. And the important thing is do you do love what you do.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  h3 {
+    font-size: 50px;
+  }
+  .pad {
+    padding-right: 150px;
+    h6 {
+    color: #20ad96;
+    text-transform: uppercase;
+  }
+  }
+.pad2 {
+    padding-right: 80px ;
+}
+span {
+    color: #20ad96;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -3,17 +3,24 @@
     <div class="row row-cols-md-4 justify-content-center mt-5">
       <InfoComponent v-for="info in infos" :title="info.title"  :number="info.num"/>
     </div>
-
+  </div>
+  <div class="container marg">
+    <div class="row">
+      <AboutMeComponent />
+    </div>
   </div>
 </template>
 
 <script>
 import InfoComponent from '../components/InfoComponent.vue';
+import AboutMeComponent from '../components/AboutMeComponents.vue'
 import { infos } from '../data/store';
 
 export default {
+  name: 'MainComponent',
   components: {
        InfoComponent,
+       AboutMeComponent,
   },
   data () {
     
@@ -26,5 +33,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+ .marg {
+  margin-top: 170px;
+ }
 </style>
